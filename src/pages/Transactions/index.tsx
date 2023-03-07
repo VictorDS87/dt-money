d64c31a4e04b24b6d56ccb228e9d7065f9cdf949
Victor Donizete <victordonizete65@gmail.com>
2023-03-07 12:02:03 -0300

    history transactions page done

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,29 @@
 import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
     return (
         <div>
             <Header/>
+            <Summary/>
+
+            <TransactionsContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <PriceHighLight variant="income">
+                                R$ 12.000,00
+                                </PriceHighLight> 
+                            </td>
+                            <td>Venda</td>
+                            <td>12/04/2002</td>
+                        </tr>
+
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
     )
 }
