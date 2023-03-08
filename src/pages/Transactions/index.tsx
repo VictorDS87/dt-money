106f26fdc9bd1b151092860f7a39cf611e2738b7
Victor Donizete <victordonizete65@gmail.com>
2023-03-08 14:34:08 -0300

    Context

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { SearchForm } from "../../components/SearchForm";
 import { Summary } from "../../components/Summary";
@@ -14,16 +15,21 @@ export function Transactions() {
                 
                 <TransactionsTable>
                     <tbody>
-                        <tr>
-                            <td>
-                                <PriceHighLight variant="income">
-                                R$ 12.000,00
-                                </PriceHighLight> 
-                            </td>
-                            <td>Venda</td>
-                            <td>12/04/2002</td>
-                        </tr>
-
+                        {transactions.map(transaction => {
+                            return (
+                                <tr key={transaction.id}>
+                                    <td width="50%">{transaction.description}</td>
+                                    <td>
+                                        <PriceHighLight variant={transaction.type}>
+                                        {transaction.price}
+                                        </PriceHighLight> 
+                                    </td>
+                                    <td>{transaction.category}</td>
+                                    <td>{transaction.createdAt}</td>
+                                </tr>
+                            )
+                        })}
+                        
                     </tbody>
                 </TransactionsTable>
             </TransactionsContainer>
